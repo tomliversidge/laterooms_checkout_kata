9faf1a53e565db4bc45e44bd337e5b72fae99a62
Tom Liversidge <tom@liversidge.me>
2016-06-27 19:52:58 +0100

    can scan remaining, non-offer items

--- CheckoutKata/Checkout.cs
+++ CheckoutKata/Checkout.cs
@@ -35,6 +35,8 @@ namespace CheckoutKata
     {
         private readonly Item _item1 = new Item("A", 50);
         private readonly Item _item2 = new Item("B", 30);
+        private readonly Item _item3 = new Item("C", 20);
+        private readonly Item _item4 = new Item("D", 15);
         
         [Fact]
         public void can_scan_a_single_item_and_get_total()
@@ -77,6 +79,20 @@ namespace CheckoutKata
             ScanItemMultipleTimes(checkout, _item1, 6); // total = 300, discount = 40
             ScanItemMultipleTimes(checkout, _item2, 4); // total = 120, discount = 30
             var expectedSubTotal = 420;
+            var expectedDiscount = 70;
+            var expectedTotal = expectedSubTotal - expectedDiscount;
+            Assert.Equal(expectedTotal, checkout.GetTotal());
+        }
+
+        [Fact]
+        public void can_scan_items_with_and_without_offers_at_checkout()
+        {
+            var checkout = new Checkout();
+            ScanItemMultipleTimes(checkout, _item1, 6); // total = 300, discount = 40
+            ScanItemMultipleTimes(checkout, _item2, 4); // total = 120, discount = 30
+            checkout.Scan(_item3); // total = 20, no discount
+            checkout.Scan(_item4); // total = 15, no discount
+            var expectedSubTotal = 455;
             var expectedDiscount = 70;
             var expectedTotal = expectedSubTotal - expectedDiscount;
             Assert.Equal(expectedTotal, checkout.GetTotal());
